b4a0f052243c23f49b5e89892eb3dcefb5d3f1b8
harikrishnan51688 <harikrishnan51688@gmail.com>
2022-01-24 19:44:56 +0530

    vul patched

--- devsearch/settings.py
+++ devsearch/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&o0ky+^*a((3yyg@gyqs1$i5skq+t01%31hf@r#7c3^o+f1n+n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
